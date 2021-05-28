fd7cdc5d53f105595fd4b01c48523ec64a4bf3c2
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-28 21:20:11 +0000

    Add initial matrix client
    Change-Id: Iaa899dc24d8ca7db76c24f41f917c2c8489ce71f

---
+++ src/Matrix.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+-- |
+module Matrix where
+
+import Control.Monad (mzero)
+import Data.Aeson
+import Data.Digest.Pure.SHA (sha1, showDigest)
+import Data.Text (Text, pack, unpack)
+import Data.Text.Encoding (encodeUtf8)
+import qualified Network.HTTP.Client as HTTP
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Relude (toLazy)
+
+data Session = Session
+  { baseUrl :: Text,
+    token :: Text,
+    manager :: HTTP.Manager
+  }
+
+createSession :: Text -> Text -> IO Session
+createSession baseUrl token = Session baseUrl token <$> HTTP.newManager tlsManagerSettings
+
+mkRequest :: Session -> Text -> IO HTTP.Request
+mkRequest Session {..} path = do
+  initRequest <- HTTP.parseUrlThrow (unpack $ baseUrl <> path)
+  pure $ HTTP.setQueryString [("access_token", Just $ encodeUtf8 token)] initRequest
+
+doRequest :: FromJSON a => Session -> HTTP.Request -> IO a
+doRequest Session {..} request = do
+  response <- HTTP.httpLbs request manager
+  case eitherDecode (HTTP.responseBody response) of
+    Right a -> pure a
+    Left err -> error err
+
+newtype WhoAmI = WhoAmI Text deriving (Show)
+
+instance FromJSON WhoAmI where
+  parseJSON (Object v) = WhoAmI <$> v .: "user_id"
+  parseJSON _ = mzero
+
+checkAccount :: Session -> IO WhoAmI
+checkAccount session = do
+  request <- mkRequest session "/_matrix/client/r0/account/whoami"
+  doRequest session request
+
+newtype RoomID = RoomID Text deriving (Show)
+
+newtype EventID = EventID Text deriving (Show)
+
+instance FromJSON EventID where
+  parseJSON (Object v) = EventID <$> v .: "event_id"
+  parseJSON _ = mzero
+
+data MessageEvent = MessageEvent
+  { meMsgtype :: Text,
+    meBody :: Text
+  }
+  deriving (Show)
+
+instance ToJSON MessageEvent where
+  toJSON msg = object ["msgtype" .= meMsgtype msg, "body" .= meBody msg]
+
+sendMessage :: Session -> RoomID -> Text -> IO EventID
+sendMessage session (RoomID roomId) body = do
+  request <- mkRequest session $ "/_matrix/client/r0/rooms/" <> roomId <> "/send/m.room.message/" <> ctx
+  doRequest
+    session
+    ( request
+        { HTTP.method = "PUT",
+          HTTP.requestBody = HTTP.RequestBodyLBS $ encode putData
+        }
+    )
+  where
+    ctx = pack . showDigest . sha1 . toLazy . encodeUtf8 $ body
+    putData = MessageEvent "m.text" body
