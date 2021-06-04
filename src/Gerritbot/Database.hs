cbd80569b3fce92a88214909b4f16dd409aff3cb
Tristan Cacqueray <tdecacqu@redhat.com>
2021-06-04 18:03:47 +0000

    Try to lookup unknown email periodically
    This change makes the bot lookup unknown email after 10 minutes.
    
    Change-Id: Ied6892a47b6ef68725dc668bc1c6287f4ba2d5cc

--- src/Gerritbot/Database.hs
+++ src/Gerritbot/Database.hs
@@ -4,9 +4,10 @@ module Gerritbot.Database where
 
 import qualified Data.HashTable.IO as HT
 import qualified Data.Text.IO as Text
+import Data.Time.Clock (UTCTime, diffUTCTime, getCurrentTime, nominalDiffTimeToSeconds)
 import Relude
 
-newtype DB = DB (HT.BasicHashTable Text (Maybe Text))
+newtype DB = DB (HT.BasicHashTable Text (Either UTCTime Text))
 
 new :: IO DB
 new = DB <$> HT.new
@@ -16,26 +17,36 @@ load fp = do
   cached <- mapMaybe toValue . lines <$> Text.readFile fp
   DB <$> HT.fromList cached
   where
-    toValue :: Text -> Maybe (Text, Maybe Text)
+    toValue :: Text -> Maybe (Text, Either UTCTime Text)
     toValue t = case words t of
-      [a] -> Just (a, Nothing)
-      [a, b] -> Just (a, Just b)
+      [a, b] -> Just (a, Right b)
       _ -> Nothing
 
 dump :: DB -> FilePath -> IO ()
 dump (DB db) fp = do
   kv <- HT.toList db
-  Text.writeFile fp (unlines . fmap toLine $ kv)
+  Text.writeFile fp (unlines . mapMaybe toLine $ kv)
   where
-    toLine :: (Text, Maybe Text) -> Text
-    toLine (name, value) = unwords [name, fromMaybe mempty value]
+    toLine :: (Text, Either UTCTime Text) -> Maybe Text
+    toLine (name, Right value) = Just $ unwords [name, value]
+    toLine (_, Left _) = Nothing
 
 get :: DB -> (Text -> IO (Maybe Text)) -> Text -> IO (Maybe Text)
 get (DB db) fetch user = do
+  now <- getCurrentTime
   v <- HT.lookup db user
   case v of
-    Just userId -> pure userId
-    Nothing -> do
+    Just (Right userId) -> pure $ Just userId
+    Just (Left date) -> do
+      if nominalDiffTimeToSeconds (diffUTCTime now date) > 600
+        then doFetch now
+        else pure Nothing
+    Nothing -> doFetch now
+  where
+    doFetch now = do
       userId <- fetch user
-      HT.insert db user userId
+      let v = case userId of
+            Just x -> Right x
+            Nothing -> Left now
+      HT.insert db user v
       pure userId
